1c9466a29e2cba4e18168a9e2afb05541f57cad2
arafathulhairiyya <arafathulhairiyya@gmail.com>
2025-05-31 12:53:07 +0530

    updates

---
+++ javascript.js
@@ -0,0 +1,5 @@
+var close = document.getElementById ("closeicon")
+var offer = document.getElementById ("offer")
+close.addEventListener ("click", function () {
+    offer.style.display = "none"
+})
