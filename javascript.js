637fba17623e5ee30ee0b5a4138846b56f0b61b3
arafathulhairiyya <arafathulhairiyya@gmail.com>
2025-06-01 21:21:36 +0530

    Delete javascript.js

--- javascript.js
+++
@@ -1,5 +0,0 @@
-var close = document.getElementById ("closeicon")
-var offer = document.getElementById ("offer")
-close.addEventListener ("click", function () {
-    offer.style.display = "none"
-})
